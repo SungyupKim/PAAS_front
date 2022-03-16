f1f92a6c7e1a3c9e969ba4b59c42786ce9fc31f4
SungyupKim <sungyupv@gmail.com>
2022-03-17 01:18:15 +0900

    get pod when login

--- src/components/pod.js
+++ src/components/pod.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios';
 import keycloak from '../keycloak';
+import { useKeycloak } from "@react-keycloak/web";
 
 const podUrl = 'http://localhost:1234/cluster/abcd/namespace/dev/pod'
 const podsReducer = (state, action) => {
@@ -30,6 +31,8 @@ const podsReducer = (state, action) => {
 };
 
 const Pods = () => {
+  const { keycloak, initialized } = useKeycloak();
+
   const [pods, dispatchPods] = React.useReducer(
     podsReducer, { data: [], isLoading: false, isError: false }
   );
@@ -37,7 +40,7 @@ const Pods = () => {
   const handleFetchPods = React.useCallback(async () => {
     dispatchPods({ type: 'PODS_FETCH_INIT' });
     try {
-      const result = await axios.get(podUrl, { headers: {"Authorization": 'Bearer ' + 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIyQzdtNjdNVDNrLU1ETm1sZld0YWE2NE42VG82azgxcVVlRVI1OXUtUklnIn0.eyJleHAiOjE2NDczNjYyNjksImlhdCI6MTY0NzM2NDQ2OSwianRpIjoiYzNkMGRmY2EtNDBhMS00ZmE5LTgxNTEtMzIyZWRhMjMwNGI4IiwiaXNzIjoiaHR0cDovLzE5Mi4xNjguMjE5LjM6ODA4MC9hdXRoL3JlYWxtcy9wYXNzIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjMyMjM4ZTUzLThkMjgtNDAxYi04YmYxLWNlMjVlYWM3NGE0MiIsInR5cCI6IkJlYXJlciIsImF6cCI6Imt1YmVjdGwtcmVzdGFwaSIsInNlc3Npb25fc3RhdGUiOiI0NmQwNjkyOS05ZTBmLTRjMmMtYTIyMS03N2E5ZTA4ZjdkNTIiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbIioiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtcGFzcyIsIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX0sImt1YmVjdGwtcmVzdGFwaSI6eyJyb2xlcyI6WyJwcm9qZWN0QTo6ZGV2ZWxvcGVyIiwidW1hX3Byb3RlY3Rpb24iXX19LCJzY29wZSI6InVtYV9wcm90ZWN0aW9uIGVtYWlsIHByb2ZpbGUiLCJzaWQiOiI0NmQwNjkyOS05ZTBmLTRjMmMtYTIyMS03N2E5ZTA4ZjdkNTIiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImdyb3VwcyI6WyIvZGV2ZWxvcGVyIl0sInByZWZlcnJlZF91c2VybmFtZSI6InN5a2ltIn0.dUEL6edJhEZD8n0LRKfe_4EgawERcceupegqv_V5PZZlGrA36F1CMXhL2DJL1KBlItMjPbeG6YTCHhuDf5ar2BSyXgSFCfZMXfj6iHTh2BCvL4-SbH3AMAmGOiBy2l9-WxcOXZ7cBF2RPEXN7JYAP4wG4FdJnP_1-OJvkI2tOjMB_nzg1G3QLJKpVsO_6y166ke1_GLfI00D8P1eJtNnOHEmiuhSF7qMWcxWfcoQRTvj9_Ffs7nNTh3aPOECgDKfJc3Gcb4nZKx3CzZhs63LpvRjLJ6GSrTxwqGduFQGFdWSnEo-mlJ1tLqTJ2XGDAGsJabmiPwTxQRaRhocbJqrkw'} });
+      const result = await axios.get(podUrl, { headers: {'Authorization': 'Bearer '  + keycloak.token}});
 
       console.log(result.data.pods)
       dispatchPods({
@@ -51,7 +54,7 @@ const Pods = () => {
   }, [podUrl]);
   React.useEffect(() => {
     handleFetchPods();
-  }, [handleFetchPods]);
+  }, [initialized]);
 
   return (
     <div>
